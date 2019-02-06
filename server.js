0620e4ea2bc8bac5a3113267e7bb46a3cd7eddf4
Hema12 <hema.ppr@gmail.com>
2019-02-06 18:14:13 +0530

    change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var path = require('path');
-
+// Change
 // import necessary js files
 var ErrorManagement = require('./server/handling/ErrorHandling.js');
 var LogManagement = require('./server/handling/LogHandling.js');
