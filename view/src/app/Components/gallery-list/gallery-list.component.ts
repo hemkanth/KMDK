7efc199697c6e5be6fe18fbb8ce458e3edfbcb14
Hema12 <hema.ppr@gmail.com>
2019-02-06 18:19:55 +0530

    ChangeView

--- view/src/app/Components/gallery-list/gallery-list.component.ts
+++ view/src/app/Components/gallery-list/gallery-list.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import {FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PageEvent } from '@angular/material';
+import { MatFormField, MatFormFieldControl } from '@angular/material';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-gallery-list',
@@ -9,8 +18,12 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
 export class GalleryListComponent implements OnInit {
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
-
-  constructor() { }
+  modalRef: BsModalRef;
+  message: string;
+  confirmbtn: Boolean = false;
+  declinebtn: Boolean = false;
+  editForm: FormGroup;
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
     this.galleryOptions = [
@@ -54,6 +67,36 @@ export class GalleryListComponent implements OnInit {
             big: 'assets/images/2.png'
         }
     ];
+    this.editForm = new FormGroup({
+        galleryname: new FormControl(null, [Validators.required, Validators.pattern('[A-Za-z]*')]),
+        galleryimage: new FormControl(null, [Validators.required])
+      });
+    }
+      openDeleteModal(deletetemplate: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(deletetemplate, {class: 'modal-sm'});
+      }
+      openEditModal(edittemplate: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(edittemplate);
+      }
+      openViewModal(viewtemplate: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(viewtemplate);
+      }
+      openCreateModal(createtemplate: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(createtemplate);
+      }
+      confirm(): void {
+        this.message = 'Confirmed!';
+        this.confirmbtn = true;
+        this.modalRef.hide();
+        setTimeout(() => {
+        }, 2000);
+      }
+      decline(): void {
+        this.message = 'Declined!';
+        this.declinebtn = true;
+        this.modalRef.hide();
+        setTimeout(() => {
+        }, 1000);
+      }
 }
 
-}
